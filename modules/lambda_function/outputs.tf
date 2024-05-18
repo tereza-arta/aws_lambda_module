98516a58f613aacab3f69dce52ee8aa13223a507
tereza-arta <157483329+tereza-arta@users.noreply.github.com>
2024-05-18 07:49:31 +0400

    Update outputs.tf

--- modules/lambda_function/outputs.tf
+++ modules/lambda_function/outputs.tf
@@ -6,12 +6,11 @@ output "lf_arn" {
     value = aws_lambda_function.tf-lambda-func.arn
 }
 
-
+/*
 output "lf_name" {
     value = aws_lambda_function.tf-lambda-func.function_name
 }
 
-/*
 output "env_var" {
     value = aws_lambda_function.tf-lambda-func.environment[0]
 }
